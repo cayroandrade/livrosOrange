fd34f52f869e8421c1584cfc5f0edf1006ad71e0
cayroandrade <99693577+cayroandrade@users.noreply.github.com>
2024-07-19 17:02:39 -0300

    Criação Projeto - evolução base - aula2

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const path = require('path');
+const mainController = require('./controller/tabelaController');
 
-// Rota raiz
-app.get('/', (req, res) => {
-    res.send('Bem-vindo ao nosso servidor Express!');
-});
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', mainController.getIndex);
+
+const port = 3000;
 
 // Iniciar o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+
+/*
+// Rota raiz
+app.get('/', (req, res) => {
+    res.send('Bem-vindo ao nosso servidor Express!');
+});
+
+ */
