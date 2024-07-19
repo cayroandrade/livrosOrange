43285daaba5f2b993040f564bd381bcffa2bdac7
cayroandrade <99693577+cayroandrade@users.noreply.github.com>
2024-07-19 18:20:16 -0300

    Criação Projeto - conectado ao mongoDb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose')
 const mainController = require('./controller/tabelaController');
 
 const app = express();
@@ -9,7 +10,19 @@ app.set('view engine', 'ejs');
 
 app.get('/', mainController.getIndex);
 
-const port = 3000;
+const port = process.env.PORT || 3000;;
+const MONGODB_URI = 'mongodb+srv://cayroandrade:MongOdb2800@fiaporange.c1jtvq7.mongodb.net/?retryWrites=true&w=majority&appName=FIAPOrange';
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão com o MongoDB:'));
+db.once('open', () => {
+    console.log('Conectado ao MongoDB');
+});
 
 // Iniciar o servidor
 app.listen(port, () => {
