6671cab2c24489f707528f5bf655fdb771acc509
cayroandrade <99693577+cayroandrade@users.noreply.github.com>
2024-07-20 13:52:11 -0300

    Criação Projeto - conectado ao mongoDb

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const MONGODB_URI = 'mongodb://localhost:27017/livrosOrange';
+//Cluster
+//const MONGODB_URI = 'mongodb+srv://cayroandrade:MongOdb2800@fiaporange.c1jtvq7.mongodb.net/?retryWrites=true&w=majority&appName=FIAPOrange/livrosOrange';
 
+//local
+const MONGODB_URI = 'mongodb://localhost:27017/livrosOrange';
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -33,6 +36,32 @@ const stockSchema = new Schema({
 
 const StockModel = mongoose.model('Stock', stockSchema);
 
+// Rota para ler todos os livros cadastrados
+app.get('/consulta', async (req, res) => {
+    try {
+        const consultaLivros = await StockModel.find();
+        res.json(consultaLivros);
+    } catch (error) {
+        res.status(500).json({ erro: 'Erro ao buscar registros de analise' });
+    }
+});
+
+// Rota para buscar um livro por id
+app.get('/consulta/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const consultaId = await StockModel.findById(id);
+
+        if (!consultaId) {
+            return res.status(404).json({ mensagem: 'Item de analise não encontrado' });
+        }
+
+        res.json(consultaId);
+    } catch (error) {
+        res.status(500).json({ erro: 'Erro ao buscar registros de analise por ID' });
+    }
+});
+
 
 // Rota para criar um novo registro de analise
 app.post('/novolivro', async (req, res) => {
@@ -53,17 +82,6 @@ app.post('/novolivro', async (req, res) => {
         res.json({ mensagem: 'Registro de analise criado com sucesso' });
     } catch (error) {
         res.status(500).json({ erro: 'Erro ao criar novolivro'});
-    }
-});
-
-
-// Rota para ler todos os registros de analise
-app.get('/consulta', async (req, res) => {
-    try {
-        const consultaLivros = await StockModel.find();
-        res.json(consultaLivros);
-    } catch (error) {
-        res.status(500).json({ erro: 'Erro ao buscar registros de analise' });
     }
 });
 
